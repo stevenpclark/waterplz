ff04459efd7f9d6e4ef4f02288b836032be518b6
Steven Clark <steven.p.clark@gmail.com>
2016-06-25 14:57:54 -0400

    playing with plots of nearby weather stations using folium/leaflet

--- waterplz.py
+++ waterplz.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 import json
 import time
 import os
+import folium
 from pprint import pprint
 
 #GET http://api.wunderground.com/api/<key>/<features>/<settings>/q/<location>.format
@@ -10,20 +11,38 @@ env = os.environ
 key = env['WATERPLZ_KEY']
 location = env['WATERPLZ_LOCATION']
 
-features = 'conditions' #multiple data features can be combined in one request
+features = 'conditions/geolookup' #multiple data features can be combined in one request
 url_format_str = 'http://api.wunderground.com/api/{}/{}/q/{}.json'
 url = url_format_str.format(key, features, location)
+print(url)
 
 f = urlopen(url)
 json_string = f.read().decode('utf-8')
 f.close()
-parsed_json = json.loads(json_string)
-pprint(parsed_json)
-#city = parsed_json['location']['city']
-#state = parsed_json['location']['state']
-#weather = parsed_json['current_observation']['weather']
-#temperature_string = parsed_json['current_observation']['temperature_string']
-#feelslike_string = parsed_json['current_observation']['feelslike_string']
-#print('Weather in ' + city + ', ' + state + ': ' + weather.lower() + '. The temperature is ' + temperature_string +
-        #' but it feels like ' + feelslike_string + '.')
+data = json.loads(json_string)
+#pprint(data)
 
+#TODO: what's the diff between current_observation's location, and the location response to geolookup
+current_obs= data['current_observation']
+obs_loc = current_obs['observation_location']
+
+obs_lat = obs_loc['latitude']
+obs_lon = obs_loc['longitude']
+
+geo_data = data['location']
+#pprint(geo_data)
+geo_lat = geo_data['lat']
+geo_lon = geo_data['lon']
+print(geo_lat, geo_lon)
+airport_stations = geo_data['nearby_weather_stations']['airport']['station']
+personal_stations = geo_data['nearby_weather_stations']['pws']['station']
+
+if 1:
+    test_map = folium.Map(location=[obs_lat, obs_lon], zoom_start=13)
+    test_map.simple_marker(location=[obs_lat, obs_lon], popup='observation')
+    test_map.simple_marker(location=[geo_lat, geo_lon], popup='geolocation')
+    for station in airport_stations:
+        test_map.simple_marker(location=[station['lat'], station['lon']], popup=station['icao'])
+    for station in personal_stations:
+        test_map.simple_marker(location=[station['lat'], station['lon']], popup=station['id'])
+    test_map.create_map(path='test.html')
